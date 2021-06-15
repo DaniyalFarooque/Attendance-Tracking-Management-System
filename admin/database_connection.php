a4c634413bfa0a6526db25d6a9681e0cf9881765
Daniyal Farooque <danf7861@gmail.com>
2021-06-15 14:36:47 +0530

    Update database_connection

--- admin/database_connection.php
+++ admin/database_connection.php
@@ -1,209 +1,222 @@
 <?php
 
-//database_connection.php
+	//database_connection.php
 
-// connect database
-$connect = new PDO("mysql:host=localhost;dbname=attendance","root","");
-
-$base_url = "http://localhost:80/Attendance-Tracking-Management-System/";
-
-// count number of total rows
-function get_total_records($connect, $table_name)
-{
-	$query = "SELECT * FROM $table_name";
-	$statement = $connect->prepare($query);
-	$statement->execute();
-	return $statement->rowCount();
-}
-
-// ------------------------------------------------------------------------------------------------------
-
-// load course list
-function load_course_list($connect)
-{
-	// select query
-	$query = "
-	SELECT * FROM tbl_course ORDER BY course_code ASC
-	";
-
-	$statement = $connect->prepare($query);
-	$statement->execute();
-	$result = $statement->fetchAll();
-	$output = '';
+	use Attendance-Tracking-Management-System\
+	// connect database
 	
-	foreach($result as $row)
+	// Development Database Connection
+	// $host = "localhost";
+	// $db = "attendance";
+	// $user = "root";
+	// $pass = "";
+	// $base_url = "http://localhost:80/Attendance-Tracking-Management-System/";
+	
+	
+	$host = "remotemysql.com";
+	$db = "YBnZN6PgeQ";
+	$user = "YBnZN6PgeQ";
+	$pass = "6aIbX19xmw";
+	$connect = new PDO("mysql:host=$host;dbname=$db",$user,$pass);
+	$base_url = "https://daniyal-atms.herokuapp.com/";
+
+	// count number of total rows
+	function get_total_records($connect, $table_name)
 	{
-		$output .= '<option value="'.$row["course_id"].'">'.$row["course_code"].'</option>';
+		$query = "SELECT * FROM $table_name";
+		$statement = $connect->prepare($query);
+		$statement->execute();
+		return $statement->rowCount();
 	}
 
-	return $output;
-}
+	// ------------------------------------------------------------------------------------------------------
 
-// -------------------------------------------------------------------------------------------------------
-
-// calculate attendance percentage
-function get_attendance_percentage($connect, $student_id)
-{
-	// select query
-	$query = "
-	SELECT 
-		ROUND((SELECT COUNT(*) FROM tbl_attendance 
-		WHERE attendance_status = 'Present' 
-		AND student_id = '".$student_id."') 
-	* 100 / COUNT(*)) AS percentage FROM tbl_attendance 
-	WHERE student_id = '".$student_id."'
-	";
-
-	// execute and fetch
-	$statement = $connect->prepare($query);
-	$statement->execute();
-	$result = $statement->fetchAll();
-	
-	// for each row calculate percentage
-	foreach($result as $row)
+	// load course list
+	function load_course_list($connect)
 	{
-		if($row["percentage"] > 0)
+		// select query
+		$query = "
+		SELECT * FROM tbl_course ORDER BY course_code ASC
+		";
+
+		$statement = $connect->prepare($query);
+		$statement->execute();
+		$result = $statement->fetchAll();
+		$output = '';
+		
+		foreach($result as $row)
 		{
-			return $row["percentage"] . '%';
+			$output .= '<option value="'.$row["course_id"].'">'.$row["course_code"].'</option>';
 		}
-		else
-		{
-			return 'NA';
-		}
+
+		return $output;
 	}
-}
 
-// --------------------------------------------------------------------------------------------------------
+	// -------------------------------------------------------------------------------------------------------
 
-// calculate percentage for defaulters
-function get_defatt_percentage($connect, $student_id)
-{
-	// select query
-	$query = "
-	SELECT 
-		ROUND((SELECT COUNT(*) FROM tbl_attendance 
-		WHERE attendance_status = 'Present' 
-		AND student_id = '".$student_id."') 
-	* 100 / COUNT(*)) AS percentage FROM tbl_attendance 
-	WHERE student_id = '".$student_id."'
-	";
-
-	// execute and fetch
-	$statement = $connect->prepare($query);
-	$statement->execute();
-	$result = $statement->fetchAll();
-	
-	// for each row calculate percentage
-	foreach($result as $row)
+	// calculate attendance percentage
+	function get_attendance_percentage($connect, $student_id)
 	{
-		if($row["percentage"] >75)
+		// select query
+		$query = "
+		SELECT 
+			ROUND((SELECT COUNT(*) FROM tbl_attendance 
+			WHERE attendance_status = 'Present' 
+			AND student_id = '".$student_id."') 
+		* 100 / COUNT(*)) AS percentage FROM tbl_attendance 
+		WHERE student_id = '".$student_id."'
+		";
+
+		// execute and fetch
+		$statement = $connect->prepare($query);
+		$statement->execute();
+		$result = $statement->fetchAll();
+		
+		// for each row calculate percentage
+		foreach($result as $row)
 		{
-			return 'NULL';			
-		}
-		else if($row["percentage"] >0)
-		{
-			return $row["percentage"] . '%';
-		}
-		else
-		{
-			return 'NA';
+			if($row["percentage"] > 0)
+			{
+				return $row["percentage"] . '%';
+			}
+			else
+			{
+				return 'NA';
+			}
 		}
 	}
-}
 
-// ------------------------------------------------------------------------------------------------------
+	// --------------------------------------------------------------------------------------------------------
 
-// get name of that student
-function Get_student_name($connect, $student_id)
-{
-	// select query
-	$query = "
-	SELECT student_name FROM tbl_student 
-	WHERE student_id = '".$student_id."'
-	";
-
-	// execute and fetch
-	$statement = $connect->prepare($query);
-	$statement->execute();
-	$result = $statement->fetchAll();
-
-	foreach($result as $row)
+	// calculate percentage for defaulters
+	function get_defatt_percentage($connect, $student_id)
 	{
-		return $row["student_name"];
+		// select query
+		$query = "
+		SELECT 
+			ROUND((SELECT COUNT(*) FROM tbl_attendance 
+			WHERE attendance_status = 'Present' 
+			AND student_id = '".$student_id."') 
+		* 100 / COUNT(*)) AS percentage FROM tbl_attendance 
+		WHERE student_id = '".$student_id."'
+		";
+
+		// execute and fetch
+		$statement = $connect->prepare($query);
+		$statement->execute();
+		$result = $statement->fetchAll();
+		
+		// for each row calculate percentage
+		foreach($result as $row)
+		{
+			if($row["percentage"] >75)
+			{
+				return 'NULL';			
+			}
+			else if($row["percentage"] >0)
+			{
+				return $row["percentage"] . '%';
+			}
+			else
+			{
+				return 'NA';
+			}
+		}
 	}
-}
 
-// -----------------------------------------------------------------------------------------------------
+	// ------------------------------------------------------------------------------------------------------
 
-// get course name from student id
-function Get_student_course_name($connect, $student_id)
-{
-	// select query
-	$query = "
-	SELECT tbl_course.course_name FROM tbl_student 
-	INNER JOIN tbl_course 
-	ON tbl_course.course_id = tbl_student.student_course_id 
-	WHERE tbl_student.student_id = '".$student_id."'
-	";
-
-	// execute and fecth
-	$statement = $connect->prepare($query);
-	$statement->execute();
-	$result = $statement->fetchAll();
-	
-	foreach($result as $row)
+	// get name of that student
+	function Get_student_name($connect, $student_id)
 	{
-		return $row['course_name'];
+		// select query
+		$query = "
+		SELECT student_name FROM tbl_student 
+		WHERE student_id = '".$student_id."'
+		";
+
+		// execute and fetch
+		$statement = $connect->prepare($query);
+		$statement->execute();
+		$result = $statement->fetchAll();
+
+		foreach($result as $row)
+		{
+			return $row["student_name"];
+		}
 	}
-}
 
-// --------------------------------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------
 
-// get faculty name from student id
-function Get_student_faculty_name($connect, $student_id)
-{
-	// select query
-	$query = "
-	SELECT tbl_faculty.faculty_name 
-	FROM tbl_student 
-	INNER JOIN tbl_course 
-	ON tbl_course.course_id = tbl_student.student_course_id 
-	INNER JOIN tbl_faculty 
-	ON tbl_faculty.faculty_course_id = tbl_course.course_id 
-	WHERE tbl_student.student_id = '".$student_id."'
-	";
-
-	// fetch and execute
-	$statement = $connect->prepare($query);
-	$statement->execute();
-	$result = $statement->fetchAll();
-	
-	foreach($result as $row)
+	// get course name from student id
+	function Get_student_course_name($connect, $student_id)
 	{
-		return $row["faculty_name"];
+		// select query
+		$query = "
+		SELECT tbl_course.course_name FROM tbl_student 
+		INNER JOIN tbl_course 
+		ON tbl_course.course_id = tbl_student.student_course_id 
+		WHERE tbl_student.student_id = '".$student_id."'
+		";
+
+		// execute and fecth
+		$statement = $connect->prepare($query);
+		$statement->execute();
+		$result = $statement->fetchAll();
+		
+		foreach($result as $row)
+		{
+			return $row['course_name'];
+		}
 	}
-}
 
-// ------------------------------------------------------------------------------------------------------
+	// --------------------------------------------------------------------------------------------------
 
-// get course name from course id
-function Get_course_name($connect, $course_id)
-{
-	// select query
-	$query = "
-	SELECT course_name FROM tbl_course 
-	WHERE course_id = '".$course_id."'
-	";
-
-	// execute and fetch
-	$statement = $connect->prepare($query);
-	$statement->execute();
-	$result = $statement->fetchAll();
-	
-	foreach($result as $row)
+	// get faculty name from student id
+	function Get_student_faculty_name($connect, $student_id)
 	{
-		return $row["course_name"];
+		// select query
+		$query = "
+		SELECT tbl_faculty.faculty_name 
+		FROM tbl_student 
+		INNER JOIN tbl_course 
+		ON tbl_course.course_id = tbl_student.student_course_id 
+		INNER JOIN tbl_faculty 
+		ON tbl_faculty.faculty_course_id = tbl_course.course_id 
+		WHERE tbl_student.student_id = '".$student_id."'
+		";
+
+		// fetch and execute
+		$statement = $connect->prepare($query);
+		$statement->execute();
+		$result = $statement->fetchAll();
+		
+		foreach($result as $row)
+		{
+			return $row["faculty_name"];
+		}
 	}
-}
+
+	// ------------------------------------------------------------------------------------------------------
+
+	// get course name from course id
+	function Get_course_name($connect, $course_id)
+	{
+		// select query
+		$query = "
+		SELECT course_name FROM tbl_course 
+		WHERE course_id = '".$course_id."'
+		";
+
+		// execute and fetch
+		$statement = $connect->prepare($query);
+		$statement->execute();
+		$result = $statement->fetchAll();
+		
+		foreach($result as $row)
+		{
+			return $row["course_name"];
+		}
+	}
 
 ?>
